9ab5a5a4d547737e840f3a8373df856a708f4950
nada <nada.mesbah@usmba.ac.ma>
2022-10-25 11:17:56 +0200

    first

--- Program.cs
+++ Program.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace essai
+namespace Github
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int n;
+            do
+            {
+                Console.WriteLine("n(>0) = ");
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n <= 0);
+            if (CheckPrime(n))
+            {
+                Console.WriteLine(n + " est un nombre premier");
+            }
+            else
+            {
+                Console.WriteLine(n + " n\'est pas un nombre premier");
+            }
+            Console.ReadLine();
         }
+        public static bool CheckPrime(int n)
+        {
+            int d;
+            bool p;
+            d = 2;
+            p = true;
+            while ((d*d <= n) && p)
+            {
+                if (n % d == 0)
+                {
+                    p = false;
+                }
+                else
+                {
+                    d++;
+                }
+            }
+            return p;
+        }
+
     }
 }
+
